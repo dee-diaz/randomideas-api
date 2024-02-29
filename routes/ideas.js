40f3f1bd717d54492b7fdcd3d55c62884582d477
dee-diaz <diazthediablo@yahoo.com>
2024-02-29 16:56:35 -0300

    Client folder setup

--- routes/ideas.js
+++ routes/ideas.js
@@ -2,44 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Idea = require('../models/Idea');
 
-const ideas = [
-    {
-        id: 1,
-        text: 'AI for personalized health plans',
-        tag: 'Technology',
-        username: 'TechGuru',
-        date: '2024-02-10'
-    },
-    {
-        id: 2,
-        text: 'Renewable energy crowdfunding platform',
-        tag: 'Innovation',
-        username: 'GreenTechie',
-        date: '2024-02-05'
-    },
-    {
-        id: 3,
-        text: 'Global online debate forum',
-        tag: 'Social',
-        username: 'DebateMaster',
-        date: '2024-01-30'
-    },
-    {
-        id: 4,
-        text: 'Crowdsourced urban planning app',
-        tag: 'Technology',
-        username: 'TechGuru',
-        date: '2024-01-18'
-    },
-    {
-        id: 5,
-        text: 'Virtual reality learning spaces',
-        tag: 'Education',
-        username: 'EduInnovator',
-        date: '2024-01-25'
-    },
-]
-
 // Get all ideas
 router.get('/', async (req, res) => {
     try {
